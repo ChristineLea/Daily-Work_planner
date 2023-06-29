597a69bc77a8702d85ea7d68dfaa098da4352b33
Christine Lea <christine87@live.com.au>
2023-06-29 19:56:27 +1000

    "Remove comments"

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,3 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// parentEl of the save button
 let $hour9 = $("#hour-9");
 let $hour10 = $("#hour-10");
 let $hour11 = $("#hour-11");
@@ -12,16 +8,6 @@ let $hour3 = $("#hour-3");
 let $hour4 = $("#hour-4");
 let $hour5 = $("#hour-5");
 let $saveBtnEl = $(".saveBtn");
-
-// let $text9 = $hour9.children().eq(1).text();
-// let $text10 = $hour10.children().eq(1).text();
-// let $text11 = $hour11.children().eq(1).text();
-// let $text12 = $hour12.children().eq(1).text();
-// let $text1 = $hour1.children().eq(1).text();
-// let $text2 = $hour2.children().eq(1).text();
-// let $text3 = $hour3.children().eq(1).text();
-// let $text4 = $hour4.children().eq(1).text();
-// let $text5 = $hour5.children().eq(1).text();
 
 $(document).ready(function () {
 	if (localStorage.getItem("nine")) {
@@ -71,11 +57,6 @@ $(document).ready(function () {
 			.text(JSON.parse(localStorage.getItem("five")));
 	}
 
-	// ar $tournament = $('#tournament'); // keep reference on element
-	// // if already data set in localstorage for this element,
-	// // set HTML element
-	// if(localStorage.getItem("#tournament")) {
-	//     $tournament.html(localStorage.getItem("#tournament"));
 	$saveBtnEl.on("click", function (event) {
 		let $btn = $(event.currentTarget).parent();
 		// let $descr = $(event.currentTarget).parent().children("textarea").text();
@@ -102,12 +83,6 @@ $(document).ready(function () {
 			localStorage.setItem("five", JSON.stringify($descr));
 		}
 	});
-
-	// TODO: Add code to apply the past, present, or future class to each time
-	// block by comparing the id to the current hour. HINTS: How can the id
-	// attribute of each time-block be used to conditionally add or remove the
-	// past, present, and future classes? How can Day.js be used to get the
-	// current hour in 24-hour time?
 
 	let currentHour = dayjs().format("h");
 	if (currentHour === 9) {
@@ -160,11 +135,6 @@ $(document).ready(function () {
 		$hour4.toggleClass("present", true).toggleClass("past", false);
 		$hour5.toggleClass("present", true).toggleClass("past", false);
 	}
-
-	//
-	// TODO: Add code to get any user input that was saved in localStorage and set
-	// the values of the corresponding textarea elements. HINT: How can the id
-	// attribute of each time-block be used to do this?
 
 	// Display TODAY
 	let currentDay = dayjs();
